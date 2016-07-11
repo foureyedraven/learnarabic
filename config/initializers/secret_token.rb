f7f143644b2a20d5aa3b9fd5963b22cb1fa07b80
Theresa Rapior <theresa.rapior@gmail.comexitxqquit>
2016-07-11 18:30:44 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learnarabic::Application.config.secret_key_base = '2fe334faf9f0633ced289d5a464e76b1c20774693afc0f1510dd7df77e46a99e1c65ba0b830c3bfa802efdfaad9c0b1326ae206cd77754ae0166ca3e74863430'
