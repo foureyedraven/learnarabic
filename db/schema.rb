2450f545ccfe5b2e0b2c674a7959241daa0ec6cf
Theresa Rapior <theresa.rapior@gmail.comexitxqquit>
2016-07-13 17:46:23 +0000

    added plan table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712200506) do
+ActiveRecord::Schema.define(version: 20160713173336) do
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
