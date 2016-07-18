c227548b39d80e6428d678cb403c7aa74c3d8e4c
Theresa Rapior <theresa.rapior@gmail.comexitxqquit>
2016-07-18 17:11:09 +0000

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Learnarabic::Application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users
+  #, controllers: { registrations: 'users/registrations' }
   resources :users do
     resource :profile
   end
